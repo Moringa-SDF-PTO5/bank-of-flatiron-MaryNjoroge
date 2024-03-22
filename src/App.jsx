8a7cb518327b8cfa2e9a85f7974d6455dd55a003
marynjoroge <wangarinjorogekenya@gmail.com>
2024-03-22 20:25:21 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,26 @@ const App = () => {
     fetchTransactions();
   }, []);
 
+  
   const fetchTransactions = () => {
     fetch(api)
-      .then(response => {
-        setTransactions(response.data);
+      .then(response => response.json()) // Parse the response as JSON
+      .then(data => {
+        setTransactions(data);
       })
       .catch(error => {
         console.error('Error fetching transactions:', error);
       });
   };
-
+  
   const addTransaction = (transaction) => {
-    fetch('http://localhost:3000/transactions', transaction)
+    fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(transaction),
+    })
       .then(() => {
         fetchTransactions(); // Refresh transactions after adding a new one
       })
@@ -31,6 +39,7 @@ const App = () => {
         console.error('Error adding transaction:', error);
       });
   };
+  
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -39,7 +48,8 @@ const App = () => {
   const filteredTransactions = transactions.filter(transaction =>
     transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
+  
+  
   return (
     <div className="App">
       <h1>Bank Transactions</h1>
